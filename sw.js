bc5eca272806644de1fa2e44bfd70cfeb5abcc39
balraic36 <116413140+balraic36@users.noreply.github.com>
2026-07-15 14:47:40 +0400

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'bible-quiz-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
+
